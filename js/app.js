2229e7ac3c5084435b98ba94dc4a22347b803929
gfed53 <gfederico7153@gmail.com>
2015-11-07 18:11:18 -0500

    Added feedback based on inputs, preventDefault on .button click, validation for input range and whole number

--- js/app.js
+++ js/app.js
@@ -1,23 +1,40 @@
 
 $(document).ready(function(){
 	console.log("Hello World");
+	var currentRand;
+	console.log(currentRand);
+	var guessCount = $('#count').html();
 
 	var newGame = function() {
 		console.log("Running game");
+		$('#feedback').html("Make your Guess!");
 		var randomNum = Math.floor(Math.random() * 100 + 1);
 		console.log(randomNum);
+		currentRand = randomNum; //Or we can do 'return randomNum'?
 
 	};
 
 	var guessCheck = function () {
 		console.log("Checking number guessed against random number");
-		var guessDiff = abs($('#userGuess').val()-randomNum);
-		if (guessDiff <= 10) {
+		var guessDiff = Math.abs($('#userGuess').val()-currentRand);
+		console.log(guessDiff);
+		if(guessDiff === 0) {
+			$('#feedback').html("You Win! Click '+New Game' to start a new game!");
+		} else if(guessDiff <= 10 && guessDiff >= 1) {
 			$('#feedback').html("Very Hot!");
+		} else if(guessDiff > 10 && guessDiff <= 20){
+			$('#feedback').html("Warm!");
+		} else if(guessDiff >20 && guessDiff <= 30){
+			$('#feedback').html("Cool");
+		} else if(guessDiff >30 && guessDiff <= 40){
+			$('#feedback').html("Cold!");
+		} else if(guessDiff > 40){
+			$('#feedback').html("Icy!");
 		}
 	};
 
 	newGame();
+	console.log(currentRand);
 
 
 	/*--- Display information modal box ---*/
@@ -31,14 +48,31 @@ $(document).ready(function(){
   		$(".overlay").fadeOut(1000);
   	});
 
-  	$('.button').on('click', function() {
-		console.log("Starts game");
+  	$('.button').on('click', function(e) {
+  		console.log(e.isDefaultPrevented());
+  		e.preventDefault();
+		console.log("Submits guess");
 		console.log($('#userGuess').val());
+		console.log(e.isDefaultPrevented());
+		if((100 >= $('#userGuess').val() > 0) && $('#userGuess').val() % 1 === 0) {
+			console.log("Good number");
+			guessCheck();
+			guessCount += 1
+		}
+		else {
+			alert("Must be a number from 1 to 100, and a whole number!");
+
+		}
+		
 	});
+
+	/*$(document).on('click', '#guessButton', function() {
+		console.log(this);
+	});*/
 
   	$('.new').on('click', function() {
   		console.log("Clicks New Game button");
-  		newGame();
+  			newGame();
   	});
 
 
